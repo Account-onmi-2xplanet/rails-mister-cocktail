373229277cd3ee09db5b96a63a54b74af758f8f0
Bruno Parente <parente.bruno@gmail.com>
2020-09-21 12:04:47 -0300

    Add dotenv - Protect my secret data in .env file

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,12 @@
 class CocktailsController < ApplicationController
     def index
         @cocktails = Cocktail.all
+        @phrases = ["Frank Sinatra said, 'Alcohol may be man's worst enemy, but the Bible says love your enemy.'",
+         "Irish Quote:'When we drink, we get drunk. When we get drunk, we fall asleep. When we fall asleep, we commit no sin. When we commit no sin, we go to Heaven. So, let's all get drunk and go to heaven.'", 
+         "Irish Quote:'What butter or whiskey does not cure cannot be cured.'", 
+         "Irish Quote:'May you always have a clean shirt, A clear conscience,And enough coins in your pocket to buy a pint.'", 
+         "Sergei Lukyanenko: 'It's about waking up in the morning with everything around you looking gray. Gray sky, gray sun, gray city, gray people, gray thoughts. And the only way out is to have another drink. Then you feel better. Then the colors come back.'"
+        ].sample
     end
     def show
         @cocktail = Cocktail.find(params[:id])
